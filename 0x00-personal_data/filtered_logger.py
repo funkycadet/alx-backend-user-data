07b9221a279335e2e8f1ad07b772db00c9c7f997
funkycadet <ericsmithsonian2@gmail.com>
2023-02-09 08:21:26 +0100

    fix: resolve pycodestye error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,13 +21,15 @@ def filter_datum(
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields=List[str]):
+    def __init__(self, fields=List[str]) -> str:
+        """ __init__ method
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
