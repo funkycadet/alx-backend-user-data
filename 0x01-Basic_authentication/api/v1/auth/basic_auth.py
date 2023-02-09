d979f553d89ddb497cab08e47f1e256a1a55c61d
funkycadet <ericsmithsonian2@gmail.com>
2023-02-09 07:17:21 +0100

    task: update extract_user method to allow password with ":"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,8 +61,9 @@ class BasicAuth(Auth):
         elif ':' not in decoded_base64_authorization_header:
             return (None, None)
         else:
-            data = decoded_base64_authorization_header.split(":")
-            return(data[0], data[1])
+            email = decoded_base64_authorization_header.split(":")[0]
+            password = decoded_base64_authorization_header[len(email) + 1:]
+            return(email, password)
 
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
