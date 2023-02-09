c9018bf60a14ce36991823fb4d05f75f507359e4
funkycadet <ericsmithsonian2@gmail.com>
2023-02-09 14:00:41 +0100

    fix: resolve pycodestye error

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,11 +4,11 @@
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """ hash_password function
     This function converts password to utf-8 encoding and hashes
     it using hashpw function from bcrypt module
     """
-    password_byte = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password_byte, salt)
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
