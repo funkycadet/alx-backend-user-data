8ee07441299f4af17842d5b7eb0a70a820669591
funkycadet <ericsmithsonian2@gmail.com>
2023-02-10 11:23:36 +0100

    task: add method to create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,12 +2,29 @@
 """ Session Authentication module
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """ SessionAuth class"""
+    """ SessionAuth class
+    """
+    user_id_by_session_id = {}
 
     def __init__(self):
         """ __init__ method
         """
         pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create_session method
+        This method creates a new session for users
+        """
+        if user_id is None:
+            return None
+        elif type(user_id) != str:
+            return None
+        else:
+            session_id = uuid.uuid4()
+            print(session_id)
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
