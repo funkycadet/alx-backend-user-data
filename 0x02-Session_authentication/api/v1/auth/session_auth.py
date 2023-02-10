ecb827e982f6577e285a385ed39c82cacdd39cf4
funkycadet <ericsmithsonian2@gmail.com>
2023-02-10 10:10:14 +0100

    task: add empty SessionAuth class for session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Session Authentication module
+"""
+from .auth import Auth
+
+class SessionAuth(Auth):
+    """ SessionAuth class"""
+
+    def __init__(self):
+        """ __init__ method
+        """
+        pass
