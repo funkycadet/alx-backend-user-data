81d4bc0c272962ba142847986363fedecc627d36
funkycadet <ericsmithsonian2@gmail.com>
2023-02-12 13:48:23 +0100

    task: add method to identify user based on session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,18 +2,15 @@
 """ Session Authentication module
 """
 from .auth import Auth
+from typing import TypeVar
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
     """ SessionAuth class
     """
     user_id_by_session_id = {}
-
-    def __init__(self):
-        """ __init__ method
-        """
-        pass
 
     def create_session(self, user_id: str = None) -> str:
         """ create_session method
@@ -35,3 +32,11 @@ class SessionAuth(Auth):
         if type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+        return user
