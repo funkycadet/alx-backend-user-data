f418b6094fa8d67925b11ddb8bc946d486f37763
funkycadet <ericsmithsonian2@gmail.com>
2023-02-19 03:01:03 +0100

    task: add method to create a user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from typing import TypeVar
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, password) -> TypeVar('User'):
+        """ add_user method
+        Creates and stores a new user in DB
+        """
+        new_user = User(email=email, hashed_password=password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
