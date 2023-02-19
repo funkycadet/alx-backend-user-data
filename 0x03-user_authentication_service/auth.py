160bd0e74572b3a6223e68dd2797bd067b73a831
funkycadet <ericsmithsonian2@gmail.com>
2023-02-19 04:46:03 +0100

    fix: change method to module function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,9 +4,10 @@
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
+from uuid import uuid4
 
 import bcrypt
-import uuid
+
 
 
 def _hash_password(password: str) -> bytes:
@@ -19,18 +20,19 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """ _generate_uuid method
+    Generates UUID
+    """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _generate_uuid(self) -> str:
-        """ _generate_uuid method
-        Generates UUID
-        """
-        return uuid.uuid4()
 
     def register_user(self, email: str, password: str) -> User:
         """ register_user method
