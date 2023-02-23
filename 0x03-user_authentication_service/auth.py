c5f4853c628c81b7cf3d2b78cdb3ce77c53de8bd
funkycadet <ericsmithsonian2@gmail.com>
2023-02-23 10:05:23 +0100

    task: add method to update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -106,3 +106,14 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Updates a user password with reference to reset_token
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        sh_pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=sh_pwd, reset_token=None)
